8f06d9e25ad89dfef149a0e06fc0892ab279f9b5
Munguss <112850918+Munguss@users.noreply.github.com>
2023-01-17 09:19:33 +0100

    YOINK

---
+++ 002/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$a = "Ik heb niet gefaald.";
+$b = "die niet werken.";
+$c = 10000;
+
+echo $a . " Ik heb alleen " . $c . " manieren gevonden " . $b;
+
+echo "<br>" . "- Thomas Edison" . "<br>";
+
+$A = "die geen fouten";
+$B = "en";
+$C = "niet.";
+$D = " ";
+
+echo "<br>" . "Mens" . $B . $D . $A . " mak" . $B . ", " . "werk" . $B . " niet.";
+
+echo "<br>" . "- Albert Plesman (KLM)" . "<br>";
+
+$aa = "ry";
+$bb = ", but";
+$cc = "fail";
+
+echo "<br>" . "T" . $aa . " and " . $cc . $bb . " never " . $cc . " to " . "t" . $aa . ".";
+
+echo "<br>" . "- Tyra Banks" . "<br>";
+
+$Aa = "\"omgaan";
+$Bb = "met";
+$Cc = "teleu";
+
+echo "<br>" . "De cursus " . $Aa . " " . $Bb . " " . $Cc . "rstellingen\"" . " kan vanavond helaas niet doorgaan.";
+
+echo "<br>" . "- Herman Finkers" . "<br>";
+
+?>
